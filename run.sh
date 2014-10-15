9821f1d55e696a9873d48b3ebe38c1772ff1c87e
PatrickWolleb <patrick.wolleb@shmedia.co.uk>
2014-10-15 14:10:05 +0100

    Fixed debug mode inverted conditional

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 LAST_DEPLOYMENT_COMMIT_HASH_FILE="$WERCKER_CACHE_DIR/last_deployed_$WERCKER_GIT_BRANCH"
 
-if [ ! -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]
+if [ -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]
 then
     warn "Debug mode turned on in git commit messages"
     cd $WERCKER_CACHE_DIR
@@ -17,7 +17,7 @@ fi
 if [ -f "$LAST_DEPLOYMENT_COMMIT_HASH_FILE" ]; then
     LAST_DEPLOYMENT_COMMIT_HASH=$(<$LAST_DEPLOYMENT_COMMIT_HASH_FILE)
     WERCKER_GIT_DIFF_MESSAGES=$(git log --abbrev-commit --pretty=oneline $LAST_DEPLOYMENT_COMMIT_HASH...HEAD)
-    if [ ! -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]; then
+    if [ -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]; then
         debug "Pulled commit hash"
         debug $LAST_DEPLOYMENT_COMMIT_HASH
         debug "Created messages"
@@ -30,7 +30,7 @@ fi
 echo $WERCKER_GIT_COMMIT > $LAST_DEPLOYMENT_COMMIT_HASH_FILE
 
 
-if [ ! -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]; then
+if [ -n "$WERCKER_GIT_COMMIT_MESSAGES_DEBUG" ]; then
     debug "New cached commit hash"
     debug $(<$LAST_DEPLOYMENT_COMMIT_HASH_FILE)
     debug "Output messages"
